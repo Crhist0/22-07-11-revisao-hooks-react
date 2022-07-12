262f2ed34be87b01e44d80d8f244a78b5c658195
Crhistian de Oliveira <crhistiandeoliveira@gmail.com>
2022-07-11 22:35:46 -0300

    temperature

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home/index';
 import Revisao from './pages/Revisao';
 import Atividade from './pages/Atividade';
 import Filtro from './pages/Filtro';
+import Temperatura from './pages/Temperatura';
 
 export default function Routers() {
     return (
@@ -12,6 +13,7 @@ export default function Routers() {
                 <Route path='/revisao' element={<Revisao/>} />
                 <Route path='/atividade' element={<Atividade/>} />
                 <Route path='/filtro' element={<Filtro/>} />
+                <Route path='/temperatura' element={<Temperatura/>} />
             </Routes>
         </BrowserRouter>
     );
