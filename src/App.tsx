4de34f0962afd72d20d043194ac06926b7dc0b23
Crhistian de Oliveira <crhistiandeoliveira@gmail.com>
2022-07-13 20:41:43 -0300

    aula redux parte 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import Routers from './Routers';
 import themeDefault from './config/themes/Default';
 import themeDark from './config/themes/Dark';
 import GlobalStyles from './config/styles/GlobalStyles';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 export default function App() {
     const theme = 1 + 1 === 2 ? themeDark : themeDefault;
     return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyles fontFamily='Arial' />
-            <Routers/>
-        </ThemeProvider>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <GlobalStyles fontFamily='Arial' />
+                <Routers/>
+            </ThemeProvider>
+        </Provider>        
     );
 };
---
+++ src/store/modules/rootReducer.tsx
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux"
+import TermostatoReducer from "./termostato/reducer"
+
+const reducers = combineReducers({
+    temperature: TermostatoReducer,
+})
+
+export default reducers
+export type State = ReturnType<typeof reducers>
