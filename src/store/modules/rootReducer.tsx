77437c9c8a10dd172c9738b064437612c2bb5941
Crhistian de Oliveira <crhistiandeoliveira@gmail.com>
2022-07-13 22:44:10 -0300

    add devtools extension e cria switch de tema

--- src/store/modules/rootReducer.tsx
+++ src/store/modules/rootReducer.tsx
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
 import TermostatoReducer from "./termostato/reducer"
+import ThemeReducer from "./theme/reducer"
 
 const reducers = combineReducers({
     temperature: TermostatoReducer,
+    theme: ThemeReducer
 })
 
 export default reducers
